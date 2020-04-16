b0b8a226168eae1554470299d7231b5f12dddabd
hashiqi12138 <liujieahu@163.com>
2020-04-16 16:35:44 +0800

    test data

--- AVMOO/mainDrive.py
+++ AVMOO/mainDrive.py
@@ -2,7 +2,7 @@ from scrapy.cmdline import execute
 
 if __name__ == "__main__":
     # execute(['scrapy', 'crawl', 'avmoo'])
-    execute(['scrapy', 'crawl', 'avmoo', '-s', 'LOG_LEVEL=INFO'])
+    execute(['scrapy', 'crawl', 'avmoo', '-s', 'LOG_LEVEL=WARNING'])
     # execute(['scrapy', 'crawl', 'avmoo', '-s', 'JOBDIR = JOB'])
     # jobbole - s
     # JOBDIR = job_info / 001
